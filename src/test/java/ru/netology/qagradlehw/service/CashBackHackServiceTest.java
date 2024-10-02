3e18eb07abe0e99871d3e7dd9ef39a19ebbcd651
Shabadam <shabadamalsu@gmail.com>
2024-10-02 17:02:43 +0300

    TestNG commit

--- src/test/java/ru/netology/qagradlehw/service/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/qagradlehw/service/CashBackHackServiceTest.java
@@ -1,16 +1,16 @@
 package ru.netology.qagradlehw.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
+import org.testng.Assert;
 
 public class CashBackHackServiceTest {
     @Test
-    void remainTest(){
+    public void remainTest(){
         CashBackHackService service = new CashBackHackService();
 
-        Assertions.assertEquals(100, service.remain(900));
-        Assertions.assertEquals(500, service.remain(500));
-        Assertions.assertEquals(0, service.remain(1000));
-        Assertions.assertEquals(250, service.remain(750));
+        Assert.assertEquals(service.remain(900), 100);
+        Assert.assertEquals(service.remain(500), 500);
+        Assert.assertEquals(service.remain(1000), 0);
+        Assert.assertEquals(service.remain(750), 250);
     }
 }
